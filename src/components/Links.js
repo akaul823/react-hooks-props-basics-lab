f0eb4a8cd1f37850decf0e6cea937a2419b153bb
Avi Kaul <avikarkaul@gmail.com>
2023-06-12 17:12:45 -0400

    react is easy

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+function Links(props){
+    return (
+        <>
+        <div>Links</div>
+        <a href={props.github}>{props.github}</a>
+        <a href={props.linkedin}>{props.linkedin}</a>
+        </>
+ 
+    )
+}
+
+export default Links
